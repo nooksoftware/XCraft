39fb468b166265b4df605bc83aea2b8f35c0f539
nooksoftware <dawidj.nook@gmail.com>
2024-11-20 20:30:08 +0100

    Library retransform, and possibly changes in game's code

--- obj/Debug/net8.0/XCraft.AssemblyInfo.cs
+++ obj/Debug/net8.0/XCraft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XCraft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aec38bf86b52ff4a9fffa7f5e50f1f9b0b7e2e45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a3727ac9ec438e29a71e78a9d7363ab0e63d8ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("XCraft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XCraft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
