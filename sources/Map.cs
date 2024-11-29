02a354f4320641560208ecfa3f012d212b12856b
nooksoftware <dawidj.nook@gmail.com>
2024-11-29 11:07:42 +0100

    rebase (2)

--- sources/Map.cs
+++ sources/Map.cs
@@ -26,7 +26,23 @@ namespace XCraft {
     };
     
     public class N {
+        public int zX = 0;
+        public int zY = 0;
+        public int zZ = 100; //50-200
+        public int zZS = 1;
+        public int nX = 0;
+        public int nY = 0;
 
+        public N() {
+            ns = new Dictionary<int, N>();
+        }
+        public Dictionary<int, N> ns;
+
+        //ver
+        public N this[int i] {
+            get {N n = null; ns.GetValueOrDefault(i, n); return n;}
+            set {if (ns[i] != null) {ns[i] = new N();} else { ns.Add(i, new N());}}
+        }
     };
     public class ER {
 
--- sources/Misc.cs
+++ sources/Misc.cs
@@ -10,5 +10,72 @@ using System.Numerics;
 //using FastNoiseLite;
 
 namespace XCraft {
-    
+    public class Ti {
+    public Ti() {}
+    };
+    public class Tf {
+        public Tf() {}
+    };
+    public class Rf {
+        public float x = 0.0f; 
+        public float y = 0.0f; 
+        public float w = 0.0f; 
+        public float h = 0.0f;
+        public Rf(float x, float y, float w, float h) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+    };
+    public class Ri {
+        public int x = 0; 
+        public int y = 0; 
+        public int w = 0; 
+        public int h = 0;
+        public Ri(int x, int y, int w, int h) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+        }
+    };
+    public class V2i {
+        public int x = 0;
+        public int y = 0;
+        public V2i(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    };
+    public class V2f {
+        public float x = 0.0f;
+        public float y = 0.0f;
+        public V2f(float x, float y) {
+            this.x = x;
+            this.y = y;
+        }
+    };
+    public class V3i {
+        public int x = 0;
+        public int y = 0;
+        public int z = 0;
+        public V3i(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+
+        }
+    };
+    public class V3f {
+        public float x = 0.0f;
+        public float y = 0.0f;
+        public float z = 0.0f;
+        public V3f(float x, float y, float z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    };
+
 }
