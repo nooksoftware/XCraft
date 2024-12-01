9b94a2e9c68b80f5adb74ae92c4fc918711f4a35
nooksoftware <dawidj.nook@gmail.com>
2024-12-01 21:02:07 +0100

    res

--- obj/Debug/net8.0/XCraft.AssemblyInfo.cs
+++ obj/Debug/net8.0/XCraft.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XCraft")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83f1f3e083c2210d1511735b22a44129c3d506b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf5a366a7db1424412d26f807301ea45da3b7a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("XCraft")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XCraft")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
